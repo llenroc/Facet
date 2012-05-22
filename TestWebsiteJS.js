f6d4187f281672f89b4229f72b21b611d08de570
unknown <Eric@SENG12.(none)>
2012-05-22 16:25:33 -0600

    Added little display thing for clicking on participants.

--- TestWebsiteJS.js
+++ TestWebsiteJS.js
@@ -1,5 +1,10 @@
-﻿
-function participantsClicked()
+﻿function participantsClicked()
 {
-	
+	var target = document.getElementById("list1");
+	if(target .style.display == "none")
+		target .style.display = "block";
+	else
+		target .style.display = "none";
+
+	return;
 }
