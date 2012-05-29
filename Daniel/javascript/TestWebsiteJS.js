c89cfa2cdd93adeffc37c74014db19ad209503c1
Daniel Sabourin <sabourid@ucalgary.ca>
2012-05-29 14:18:26 -0600

    Made the workspace panel horizontal and started adding ability to add users to list

--- Daniel/javascript/TestWebsiteJS.js
+++ Daniel/javascript/TestWebsiteJS.js
@@ -17,3 +17,20 @@ function hideAll()
 	hide('list2');
 
 }
+
+function addUser2(name, email)
+{
+	var list = document.getElementById('participantList');
+	var newLi = document.createElement("LI");
+	newLi.innerHTML = "<a href=mailto:" + email + ">" + name + "</a>";
+	newLi.className = "user";
+	list.appendChild(newLi);
+}
+
+function addUser() 
+{
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	
+	addUser2(name,email);
+}
