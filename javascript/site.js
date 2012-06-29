bcf37babe97030e71eb9227f68830e60caaf8048
Daniel Sabourin <sabourid@ucalgary.ca>
2012-06-29 11:05:32 -0600

    Queue arrow test

--- javascript/site.js
+++ javascript/site.js
@@ -57,7 +57,9 @@ $(function() {
 	});
 	
 	$(".queue").scroll(function() {
-
+		if($(this).scrollTop() > 0) {
+			$(this).scrollTop(0);
+		}
 	    if($(this)[0].scrollWidth - $(this).scrollLeft()-500 <= $(this).outerWidth())
 		{
 			// At right
