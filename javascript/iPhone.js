ee66a80f7526cda80c376d60fcdb5c123d289c77
Daniel Sabourin <sabourid@ucalgary.ca>
2012-07-04 12:44:35 -0600

    Added deletion of users

--- javascript/iPhone.js
+++ javascript/iPhone.js
@@ -1,3 +1,5 @@
+var selectedName;
+
 $(function() {
 	var myOptions = {
 		center: new google.maps.LatLng(49.891235,-97.15369),
@@ -5,14 +7,24 @@ $(function() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);	
+	
+	$("#participantList li").click(function() {
+		selectedName = $(this);
+		$("#optionHeader").text(selectedName.text());
+	});
 });
 
 $.fn.newGroup = function() {
 	var name = $("#createGroupLabel").val();
-	$("#createGroupLabel").val("");
 	if(name != "") {
 		$("#groupList").append("<li><a>"+ name + "</a><ul data-role='listview' data-inset='true'></ul></li>");
 		$('#groupList').listview('refresh');
 		$('#newgroupDialog').dialog('close');
+		$("#createGroupLabel").val("");
 	}
+};
+
+$.fn.deleteUser = function() {
+	$("#participantList").children().slice(selectedName.index(),selectedName.index()+1).remove();
+	$('#participantDialog').dialog('close');
 };
