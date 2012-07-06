6d28e72426a795995623ff6960d96a394654462a
patrick.f.king@gmail.com <patrick.f.king@gmail.com>
2012-07-06 15:29:02 -0600

    New ajax functions

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -5,15 +5,16 @@
 PS.ajax = {};
 
 if( typeof phpServer === 'undefined') {
-	PS.useCurlMirror = true;
+	PS.useReverseProxy = true;
 }
 else {
-	PS.useCurlMirror = false;
+	PS.useReverseProxy = false;
 }
 
 PS.ajax.getServerPrefix = function() {
-	if(PS.useCurlMirror) {
-		return "http://localhost:81/curl.php?request="
+	if(PS.useReverseProxy) {
+
+		return "http://localhost:81/facet/";
 	}
 	else {
 		return phpServer;
@@ -22,26 +23,88 @@ PS.ajax.getServerPrefix = function() {
 
 
 // ajax calls
+/***** User *****/
+
+//TODO: Local check that user is logged in before accessing secured methods
+//Unsecured: login, create
+//Secured: logout, index, update, everything else ... ?
 
 PS.ajax.userLogin = function (username, password, callback) {
 	$.ajax({
 		type: "POST",
-		url: PS.ajax.getServerPrefix() + "http://localhost:81/drupal-7.14/page/user/login/", //replace with actual URL
+		url: PS.ajax.getServerPrefix() + "restfacet/user/login",
 		dataType: "json",
-		success: callback,
-		data: {username: username,
-		password: password}
+		success: function(data, textStatus, jqXHR) { PS.ajax.setCookie(data, textStatus, jqXHR); callback(data, textStatus, jqXHR); },
+		data: {username: username, password: password}
 		
 	});
 }
 
-
-
-
 PS.ajax.userLogout = function (callback) {
 	$.ajax({
 		type: "POST",
-		url: PS.ajax.getServerPrefix() + "http://localhost:81/drupal-7.14/page/user/logout/", //replace with actual URL
+		url: PS.ajax.getServerPrefix() + "restfacet/user/logout/",
+		dataType: "json",
+		success: callback, // TODO: Delete cookie after logging out.
+	});
+}
+
+//Not an ajax call, just a local utility function. 
+//TODO: Only needs to be called when using reverse proxy for local development ... 
+PS.ajax.setCookie = function (data, textStatus, jqXHR) {
+		session = data;
+		var today = new Date();
+		var expire = new Date();
+		expire.setTime(today.getTime() + 3600000*24*30); // 30 days
+		document.cookie = session.session_name+"="+escape(session.sessid)
+			+ ";expires="+expire.toGMTString();
+}
+    
+PS.ajax.userRetrieve = function(userId, callback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "restfacet/user/" + userId,
+		dataType: "json",
+		success: callback,
+	});
+}
+
+PS.ajax.userCreate = function(username, email, password, callback) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/user/",
+		dataType: "json",
+		success: callback,
+		data: {name: username, mail:email, pass: password}, // all three required
+	});
+}
+
+
+PS.ajax.userUpdate = function(userId, data, callback) {
+	$.ajax({
+		type: "PUT",
+		url: PS.ajax.getServerPrefix() + "restfacet/user/" + userId,
+		dataType: "json",
+		success: callback,
+		data: data,
+	});
+}
+
+
+PS.ajax.userDelete = function(userId, callback) {
+	$.ajax({
+		type: "DELETE", 
+		url: PS.ajax.getServerPrefix() + "restfacet/user/" + userId,
+		dataType: "json",
+		success: callback,
+	});
+}
+
+
+PS.ajax.userIndex(event, callback) {
+	$.ajax({
+		type: "GET", // or PUT or DELETE, oddly enough
+		url: PS.ajax.getServerPrefix() + "restfacet/user/",
 		dataType: "json",
 		success: callback,
 	});
@@ -49,14 +112,49 @@ PS.ajax.userLogout = function (callback) {
 
 
 
-PS.ajax.getSurveys = function(callback) {
+
+/***** Survey *****/
+
+PS.ajax.surveyIndex = function(callback) { // getSurveys
 	$.ajax({
 		type: "GET",
-		url: PS.ajax.getServerPrefix() + "facetsurvey.4abyte.com/json-services/",
+		url: PS.ajax.getServerPrefix() + "json-services/",
 		dataType: "json",
 		success: callback
 	});
 };
+
+
+PS.ajax.surveyRetrieve = function(surveyId, callback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "json-services/" + surveyId,
+		dataType: "json",
+		success: callback
+	});
+};
+
+
+
+/***** Nodes *****/
+//TODO: Drupal rest also supports node functions... do we need these?
+
+
+PS.ajax.nodeIndex(callback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+	});
+}
+
+
+
+
+
+
+
 
 
 
