e65f0fadad06619e44859add133661fbc26dfbd9
sabourid <sabourid@ucalgary.ca>
2012-07-16 15:28:38 -0600

    Testing cookies

--- javascript/site.js
+++ javascript/site.js
@@ -28,7 +28,7 @@ $(function() {
 	var scrollable4 = document.getElementById("queue");
 	new ScrollFix(scrollable4);
 	
-	console.log(document.cookie);
+	console.log(document.cookie.split(";"));
 	
 	
 	$("#columns .column header h1 img.dragHandle2").hover(function(){$(this).parent().parent().parent().addClass("hover-border2");}, function () {$(this).parent().parent().parent().removeClass("hover-border2");});
