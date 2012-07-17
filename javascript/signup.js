6305cca2a425f64577ec1c2f30b052a1180e6455
nmakhlou <chefjoepharaoh@hotmail.com>
2012-07-17 16:35:17 -0600

    Re-ADDED password fields to signup page after lots of discussion of what should we have on this new web page

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,34 +1,47 @@
 ﻿var emailError = false;
+var pwError = false;
 
 function signup() {
-    $('#emailError').css("display", "none");
+    $("#pwError").css("display", "none");
+    $("#emailError").css("display", "none");
     $("#emptyError").css("display", "none");
     $("#emailUsed").css("display", "none");
     $("#processing").css("display", "none");
 
     var username = $("#username").val();
     var email = $("#email").val();
-    var passed = checkCredentials(email);
+    var pw = $("#password").val();
+    var passed = checkCredentials(email, pw);
 
     if (passed) {
         $("#processing").css("display", "block");
-        PS.ajax.userCreate(username, email, "", userCreated, userCreateFailed);
+        PS.ajax.userCreate(username, email, pw, userCreated, userCreateFailed);
     }
     else {
         failedCreation("");
     }
 };
 
-function checkCredentials (email) {
+function checkCredentials (email, pw) {
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var spaces = pw.split(" ");
     emailError = true;
+    pwError = true;
 
-    if (filter.test(email)) {
-        console.log("Email passed standard format check");
-        emailError = false;
-        if ($("#email").val() != "" && $("#username").val() != "" && $("#fname").val() != "") {
-            console.log("Credentials are not empty");
-            return true;
+    if (pw.valueOf() == $("#cpassword").val()) {
+        if (pw.length > 5) {
+            if (spaces.length == 1) {
+                console.log("Password passed basic check");
+                pwError = false;
+                if (filter.test(email)) {
+                    console.log("Email passed standard format check");
+                    emailError = false;
+                    if ($("#email").val() != "" && $("#username").val() != "" && $("#fname").val() != "") {
+                        console.log("Credentials are not empty");
+                        return true;
+                    }
+                }
+            }
         }
     }
     return false;
@@ -37,7 +50,12 @@ function checkCredentials (email) {
 function failedCreation (jqXHR) {
     console.log("User not created");
     $("#processing").css("display", "none");
-    if (jqXHR != "") {
+
+    if (pwError || jqXHR.indexOf("Not Acceptable: Password field is required") != -1) {
+        $("#pwError").css("display", "block");
+        pwError = false;
+    }
+    else if (jqXHR.indexOf("Not Acceptable: The e-mail address") != -1) {
         $("#emailUsed").css("display", "block");
     }
     else if (emailError) {
