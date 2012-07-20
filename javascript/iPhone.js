74577bb408e144bb8e752db7227b835e73bc3e41
sabourid <sabourid@ucalgary.ca>
2012-07-20 15:50:35 -0600

    Reading up on jQuery mobile documentation and they say to use pageinit instead of document.ready. I've changed the code structure a bit to use pageinit, and so hopefully some problems I've been dealing with trying to refresh a listview when it hasn't been initialized will be easier to deal with

--- javascript/iPhone.js
+++ javascript/iPhone.js
@@ -2,8 +2,10 @@
 var selectedItem;
 var selectedGroup;
 
-$(function() {
-
+// Everything to do with elements in the workspace goes here
+$("#workspace").live('pageinit', function() {
+	console.log("Workspace");
+	
 	// Populate the workspace with some sample items
 	$(this).createItem('audio', "empty.html", "Audio");
 	$(this).createItem('file', "empty.html", "File");
@@ -12,8 +14,21 @@ $(function() {
 	$(this).createItem('file', "empty.html", "File");
 	$(this).createItem('file', "empty.html", "File");
 	$(this).createItem('audio', "empty.html", "Audio");
-	$(this).createItem('images', "empty.html", "Image"); 
+	$(this).createItem('images', "empty.html", "Image");
 	
+	$('#workspaceList').listview('refresh', true);
+	
+});
+
+// Everything to do with elements in the workspace goes here
+$("#queue").live('pageinit', function() {
+	console.log("Queue");
+	
+});
+	
+// Everything to do with elements in the workspace goes here
+$("#map").live('pageinit', function() {
+	console.log("Map");
 	
 	// Initialize the Google Map to be zoomed in on the lat/long
 	var myOptions = {
@@ -22,55 +37,67 @@ $(function() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);	
-		
 	
-	// Saves the li to selectedItem to be used later on
-	$("#participantList li, #workspaceList li").live("click", function() {
-		selectedItem = $(this);
-		
-		// Change header to be more approprate for the item that was clicked on
-		$(".dialogHeader").text(selectedItem.text());
-	});
-	
-	// When clicking on an item from the group dialog that isn't a header, this gets called. It formats the string to get rid of trailing spaces and then adds the user to the selected group.
-	$(".groupListPop li:not([data-role='list-divider'])").live("click", function() {
-		selectedGroup = $(this);
-		var text = selectedGroup.text();
-		var text2 = text.substring(0,text.length-1);
-		$(this).addUserToGroup(selectedItem.text(),text2);
-		
-		// Go back 2 dialog boxes (to the main screen)
-		window.history.go(-2);
-		
-	});
-				
-	// A fix to an error I was having i f I tried to refresh the dialog list
-	$('#addToGroupDialog').on('pagecreate pageshow', function (event) {
+});
 
-		var $the_ul = $('.groupListPop');
-        if ($the_ul.hasClass('ui-listview')) {
-            $the_ul.listview('refresh');
-        } else {
-            $the_ul.trigger('create');
-        }
-    });
-				
-	// Hiding function
-	$(".group").live("click", function() {
-		// .hidden is used for when adding users to a group. If a user is added to a group that is hidden, the users is hidden
-		if($(this).hasClass("hidden")) {
-			$(this).removeClass("hidden");
-		} else {
-			$(this).addClass("hidden");
-		}
-		
-		// Goes from the item clicked up until the next divider and toggles visibility
-		$(this).nextUntil(".ui-li-divider").toggle();
-	});
+// Everything to do with elements in the workspace goes here
+$("#sharedScreen").live('pageinit', function() {
+	console.log("Shared Screen");
 	
+});
+	
+// Everything to do with elements in the workspace goes here
+$("#participants").live('pageinit', function() {
+	console.log("Participants");
+						
 	$("#participantList").append("<li id='loading'><a>Loading Users...</a></li>");
 	$('#participantList').listview('refresh', true);
 	PS.ajax.userIndex(populateParticipants, populateFailed);		
+});
+
+
+// Saves the li to selectedItem to be used later on
+$("#participantList li, #workspaceList li").live("click", function() {
+	selectedItem = $(this);
+	
+	// Change header to be more approprate for the item that was clicked on
+	$(".dialogHeader").text(selectedItem.text());
+});
+
+// When clicking on an item from the group dialog that isn't a header, this gets called. It formats the string to get rid of trailing spaces and then adds the user to the selected group.
+$(".groupListPop li:not([data-role='list-divider'])").live("click", function() {
+	selectedGroup = $(this);
+	var text = selectedGroup.text();
+	var text2 = text.substring(0,text.length-1);
+	$(this).addUserToGroup(selectedItem.text(),text2);
+	
+	// Go back 2 dialog boxes (to the main screen)
+	window.history.go(-2);
+	
+});
+
+// A fix to an error I was having if I tried to refresh the dialog list
+$('#addToGroupDialog').on('pagecreate pageshow', function (event) {
+
+	var $the_ul = $('.groupListPop');
+	if ($the_ul.hasClass('ui-listview')) {
+		$the_ul.listview('refresh');
+	} else {
+		$the_ul.trigger('create');
+	}
+});
+
+// Hiding function
+$(".group").live("click", function() {
+	// .hidden is used for when adding users to a group. If a user is added to a group that is hidden, the users is hidden
+	if($(this).hasClass("hidden")) {
+		$(this).removeClass("hidden");
+	} else {
+		$(this).addClass("hidden");
+	}
+	
+	// Goes from the item clicked up until the next divider and toggles visibility
+	$(this).nextUntil(".ui-li-divider").toggle();
 });
 
 function createUser(name, id) {
