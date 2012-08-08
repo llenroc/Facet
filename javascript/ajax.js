e2599fc564b0477446b05141b516870e16288b20
patrick.f.king@gmail.com <patrick.f.king@gmail.com>
2012-08-08 14:19:46 -0600

    new ajax methods and tests

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -164,10 +164,10 @@ PS.ajax.surveyRetrieve = function(surveyId, callback) {
 
 
 
-/***** Nodes *****/
-//TODO: Drupal rest also supports node functions... do we need these?
+/***** Node *****/
+// Since the 4abyte objects are all nodes, we can use certain generic functions.
 
-
+// Node index, generally not useful.
 PS.ajax.nodeIndex = function(callback) {
 	$.ajax({
 		type: "GET",
@@ -177,6 +177,41 @@ PS.ajax.nodeIndex = function(callback) {
 	});
 }
 
+//PS.ajax.nodeCreate
+
+PS.ajax.nodeRetrieve = function(callback, errorCallback, id) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/" + id,
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});
+}
+
+PS.ajax.nodeUpdate = function(callback, errorCallback, id, data) {
+	$.ajax({
+		type: "PUT",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/" + id,
+		dataType: "json",
+		success: callback,
+		error: error,
+		data: data,
+	});
+}
+
+PS.ajax.nodeDelete = function(callback, errorCallback, id) {
+	$.ajax({
+		type: "DELETE",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/" + id,
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});
+}
+
+
+/***** Twitter *****/
 
 PS.ajax.tweet = function(hashtag,location,name,type,filename) {
 	$.ajax({
@@ -192,6 +227,251 @@ PS.ajax.tweet = function(hashtag,location,name,type,filename) {
     });
 }
 
+
+
+/***** PublicSquare Objects *****/
+//NB: There is basically no type checking or validity checking on the server site. And as of this writing, next to none on the client either. Here be dragons.
+
+//Technically, you can set relationships like any other attribute using the below defined update functions. This is not a good idea. Use the relationship functions instead. 
+
+// Helper functions
+PS.ajax.wrapNodeId(input) {
+	return "[nid:" + input + "]" 
+}
+
+
+
+// Project
+PS.ajax.projectIndex = function(callback, errorCallback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/project",
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});	
+}
+// userNodeId: the userNode for a user who will own this project
+PS.ajax.projectCreate = function(callback, errorCallback, name, userNodeId) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+        error: errorCallback,
+		type: 'project'
+		data: {name: name, 'field_owners[und][0][nid]': PS.ajax.wrapNodeId(userNodeId)}
+	});
+}
+PS.ajax.projectRetrieve = PS.ajax.nodeRetrieve;
+//PS.ajax.projectUpdate = PS.ajax.nodeUpdate;
+PS.ajax.projectDelete = PS.ajax.nodeDelete;
+
+
+
+// Meeting
+PS.ajax.meetingIndex = function(callback, errorCallback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/meeting",
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});		
+}
+// userNodeId: the userNode for a user who will own this meeting
+// projectId: id of the project node that owns the meeting
+// date, startTime, endTime, hashTag: strings
+PS.ajax.meetingCreate = function(callback, errorCallback, name, userNodeId, projectId, date, startTime, endTime, hashTag) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+        error: errorCallback,
+		type: 'meeting'
+		data: {	name: name, 
+				'field_meeting_owners[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+                'field_meeting_owning_project[und][0][nid]': PS.ajax.wrapNodeId(projectId),			
+				'field_meeting_date[und][0][nid]': date,
+				'field_meeting_start_time[und][0][nid]': startTime,
+				'field_meeting_end_time[und][0][nid]': endTime,
+				'field_meeting_hashtag[und][0][nid]': hashTag,
+		}
+	});	 
+}
+PS.ajax.meetingRetrieve = PS.ajax.nodeRetrieve;
+//PS.ajax.meetingUpdate = PS.ajax.nodeUpdate;
+PS.ajax.meetingDelete = PS.ajax.nodeDelete;
+
+
+// Group
+PS.ajax.groupIndex = function(callback, errorCallback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/group",
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});		
+}
+PS.ajax.groupCreate = function(callback, errorCallback, name, userNodeId, projectId) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+        error: errorCallback,
+		type: 'group'
+		data: {	name: name, 
+				'field_group_owners[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+				'field_group_owning_project[und][0][nid]': PS.ajax.wrapNodeId(projectId),
+		}
+	});	
+}
+PS.ajax.groupRetrieve = PS.ajax.nodeRetrieve;
+//PS.ajax.groupUpdate = PS.ajax.nodeUpdate;
+PS.ajax.groupDelete = PS.ajax.nodeDelete;
+
+// Item
+PS.ajax.itemIndex = function(callback, errorCallback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/item",
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});		
+}
+//userNodeId: node id for the user that created and initially owns the item
+//createdTime: a string
+//TODO: Add support for item type. item may need to have a new node reference field added to refer to arbitrary external nodes ... as well as additional fields for the text item types (comment, question, note/annotation)
+PS.ajax.itemCreate = function(callback, errorCallback, name, userNodeId, createdTime) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+        error: errorCallback,
+		type: 'item'
+		data: {	name: name, 
+				'field_item_id_owner[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+				'field_item_id_creator[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+				'field_item_creation_timestamp[und][0][nid]': createdTime,
+				'field_item_shared_timestamp[und][0][nid]': '',
+				//'field_item_type[und][0][nid]': 'survey',
+		}
+	});		
+}
+PS.ajax.itemRetrieve = PS.ajax.nodeRetrieve;
+//PS.ajax.itemUpdate = PS.ajax.nodeUpdate;
+PS.ajax.itemDelete = PS.ajax.nodeDelete;
+
+
+// User Node 
+PS.ajax.userNodeIndex = function(callback, errorCallback) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/user",
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});		
+}
+
+{userName:'stuff'}
+
+//name: user's full name
+//userName: must be the username of an existing user. 
+PS.ajax.userNodeCreate = function(callback, errorCallback, name, userName) {
+	$.ajax({
+		type: "POST",
+		url: PS.ajax.getServerPrefix() + "restfacet/node/",
+		dataType: "json",
+		success: callback,
+        error: errorCallback,
+		type: 'user'
+		data: {	name: name, 
+				'field_user_name[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+				'field_user_username[und][0][nid]': PS.ajax.wrapNodeId(userNodeId),
+				'field_user_logged_in[und][0][nid]': 'absent', // 4abyte specs say this should be 'present' or 'absent'. don't know if there is any logic hooked up to it on their end.
+		}
+	});			
+}
+PS.ajax.userNodeRetrieve = PS.ajax.nodeRetrieve;
+//PS.ajax.userNodeUpdate = PS.ajax.nodeUpdate;
+PS.ajax.userNodeDelete = PS.ajax.nodeDelete;
+
+
+
+
+
+
+
+/***** PublicSquare Relationships *****/
+// 'REST' methods available for each relationship: create, delete, one-way index.
+// create/delete are implemented by an update to the object in the relation that 'owns' the relation: the an update to hte object having the field that refers to the other object.
+// NB / TODO: Because relations are implemented as arrays, and creation/deletion is done with array indices, these operations are NOT safe for concurrent operations. 
+// NB: services-xml calls are not handled by drupal services, they ignore dataType and return only xml
+
+//TODO: Convenience function to decode relation members from objects
+
+//user_project_member
+
+PS.ajax.indexUserProjects = function(callback, errorCallback, userNodeId) {
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "services-xml/project/user/" + userNodeId,
+		success: callback,
+		error: errorCallback,
+	});		
+}
+PS.ajax.addProjectUser = function(callback, errorCallback, projectId, userNodeId) {
+	
+	PS.ajax.nodeRetrieve(
+		function(json, textStatus, jqXHR) {
+		
+			data = {};
+
+			PS.ajax.nodeUpdate(callback, errorCallback, projectId, data)
+		
+
+		},
+		errorCallback,
+		projectId,
+	);
+
+	$.ajax({
+		type: "GET",
+		url: PS.ajax.getServerPrefix() + "" + projectId,
+		dataType: "json",
+		success: callback,
+		error: errorCallback,
+	});	
+}
+PS.ajax.removeProjectUser = function(callback, errorCallback, projectId, userNodeId) {
+	
+}
+
+//user_meeting_member
+//user_group_member
+
+//user_project_owner
+//user_meeting_owner
+//user_group_owner
+
+//user_project_observer
+//user_meeting_observer
+//user_group_observer
+
+//item_creator
+//item_owner
+
+//meeting_queue
+//meeting_active_item
+//item_payload
+//project_group
+//project_meeting
 
 
 
